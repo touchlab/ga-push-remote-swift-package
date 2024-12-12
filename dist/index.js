f5840c1c2f61e95723029439e2c493d39c4d00c4
Kevin Galligan <kgalligan@gmail.com>
2024-12-11 20:09:54 -0500

    Update compiled js

--- dist/index.js
+++ dist/index.js
@@ -31260,7 +31260,7 @@ async function run() {
         else {
             await worktreeGit.add('.');
             await worktreeGit.commit(commitMessage);
-            await worktreeGit.raw('push', remoteRepoUrl, remoteBranch);
+            await worktreeGit.raw('push', remoteRepoUrl, `remote_swift_package:${remoteBranch}`);
         }
     }
     catch (error) {
